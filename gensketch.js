0c3343f0ee4aa65f6a08c55c8d6ebf10df140499
marikalss <119530149+marikalss@users.noreply.github.com>
2023-04-28 19:47:55 +0100

    Update gensketch.js
    Number limit and final (?) code

--- gensketch.js
+++ gensketch.js
@@ -1,42 +1,34 @@
-let state = 0;
 let number = 0;
 let firstDirections = ["go left", "go right"];
 let directions = ["take the next right", "take the next left", "keep straight"];
 let textHeight = 125;
 let textHeightc2 = 125;
 let textHeightc3 = 125;
+let textHeightc4 = 125;
 let dirNumber = 0;
 let buttonGen, buttonRand, inputDir, buttonRestart, randomGen, randomList1;
 let randomList = [];
-
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   background(255);
   userInput();
-  //background(255);
 }
 
 function restartSketch() {
-  
-  
-  
   color("white");
   square(0, 0, windowWidth, windowHeight);
   background(255);
-  state = 0;
+
   number = 0;
-  //firstDirections = ['go left', 'go right'];
-  //directions = ['take the next right', 'take the next left', 'keep straight'];
   textHeight = 125;
   textHeightc2 = 125;
   textHeightc3 = 125;
+  textHeightc4 = 125;
   dirNumber = 0;
   buttonRestart.hide();
   randomGen.hide();
   print("restart");
-  //randomList.hide();
-  //randomList1.hide();
   setup();
 }
 
@@ -44,56 +36,51 @@ function draw() {}
 
 function userInput() {
   background(255);
+
+  col = color(255);
+
+  print("InputGoing!!");
+  inputDir = createInput();
+  inputDir.style("border-width:2px");
+  inputDir.style("border-color: black");
+  inputDir.position(50, 100);
+  inputDir.size(300, 25);
+  inputDir.input(isNumber);
+
+  buttonGen = createButton("generate");
+  buttonGen.style("background-color", col);
+  buttonGen.style("border-width:2px");
+  buttonGen.style("border-color: black");
+  buttonGen.style("font-family", "proxima-nova");
+  buttonGen.style("font-weight", "bold");
+  buttonGen.size(100, inputDir.height);
+  buttonGen.position(
+    inputDir.x + inputDir.width - buttonGen.width,
+    inputDir.y + buttonGen.height + 25);
+
+  buttonRand = createButton("random");
+  buttonRand.style("background-color", col);
+  buttonRand.style("border-width:2px");
+  buttonRand.style("font-family", "proxima-nova");
+  buttonRand.style("font-weight", "bold");
+  buttonRand.style("border-color: black");
+  buttonRand.size(100, inputDir.height);
+  buttonRand.position(inputDir.x, inputDir.y + buttonRand.height + 25);
+
+  greeting = createElement("h1", "how many directions?");
+  greeting.position(inputDir.x, inputDir.y - 60);
+
+  max = createElement("p", "max 100");
+  max.position(inputDir.x+inputDir.width-45, inputDir.y+20);
   
-  if (state == 0) {
-    col = color(255);
+  min = createElement("p", "min 1");
+  min.position(inputDir.x, inputDir.y+20);
 
-    print("InputGoing!!");
-    inputDir = createInput();
-    inputDir.style("border-width:2px");
-    inputDir.style("border-color: black");
-    inputDir.position(50, 100);
-    inputDir.size(300, 25);
-    inputDir.input(isNumber)
- 
-    
+  
+ buttonGen.mousePressed(inputVal);
 
-    // inputDir.focus('outline:none')
-
-    buttonGen = createButton("generate");
-    buttonGen.style("background-color", col);
-    buttonGen.style("border-width:2px");
-    buttonGen.style("border-color: black");
-    buttonGen.style("font-family", "proxima-nova");
-    buttonGen.style("font-weight", "bold");
-    buttonGen.size(100, inputDir.height);
-    buttonGen.position(
-      inputDir.x + inputDir.width - buttonGen.width,
-      inputDir.y + buttonGen.height + 20
-    );
-
-    buttonRand = createButton("random");
-    buttonRand.style("background-color", col);
-    buttonRand.style("border-width:2px");
-    buttonRand.style("font-family", "proxima-nova");
-    buttonRand.style("font-weight", "bold");
-    buttonRand.style("border-color: black");
-    buttonRand.size(100, inputDir.height);
-    buttonRand.position(inputDir.x, inputDir.y + buttonRand.height + 20);
-
-    // textSize(25);
-    // greeting = text('how many directions?', inputDir.x, inputDir.y-25)
-
-    greeting = createElement("h1", "how many directions?");
-    greeting.position(inputDir.x, inputDir.y - 60);
-
-
-          buttonGen.mousePressed(inputVal);
- 
-
-    buttonRand.mousePressed(randomInput);
-    state = 1;
-  }
+  
+ buttonRand.mousePressed(randomInput);
 }
 
 function isNumber() {
@@ -101,32 +88,39 @@ function isNumber() {
   let c = s.charCodeAt(s.length - 1);
 
   if (!(c > 47 && c < 58)) {
-    this.value(s.substring(0,s.length-1));
+    this.value(s.substring(0, s.length - 1));
     textFont("Proxima Nova");
-  } 
+  }
 }
 
 function inputVal() {
   dirNumber = inputDir.value();
+  if(dirNumber>=1 && dirNumber<=100){
   randomGen = createElement("h1", dirNumber + " directions generated");
   randomGen.position(greeting.x, greeting.y);
   generate();
+  }
+  else{
+  buttonGen.hide();
+  buttonRand.hide();
+  greeting.hide();
+  inputDir.hide();
+  max.hide();
+  min.hide();
+  userInput();
+  }
 }
 
 function randomInput() {
-  dirNumber = int(random(5, 50));
+  dirNumber = int(random(1, 100));
 
   randomGen = createElement("h1", dirNumber + " directions generated");
   randomGen.position(greeting.x, greeting.y);
-
-  //   textSize(24)
-  //   text(dirNumber + ' directions generated', greeting.x, greeting.y)
 
   generate();
 }
 
 function generate() {
-  
   print("generator going!");
   textFont("Proxima Nova");
 
@@ -134,47 +128,38 @@ function generate() {
   buttonRand.hide();
   greeting.hide();
   inputDir.hide();
+  max.hide();
+  min.hide();
 
   if (number < 1) {
     number = number + 1;
     let firstDirection = random(firstDirections);
 
     textSize(12);
-    
-    text(firstDirection, 100, textHeight);
-      //textHeight = textHeight + 20;
-     // state = 3;
-    
-    // randomList1 = createElement("p", firstDirection);
-    // randomList1.position(100, textHeight);
-    //text(firstDirection, greeting.x, textHeight);
+
+    text(firstDirection, 50, textHeight);
     textHeight = textHeight + 20;
   }
 
-  // if (number <=dirNumber){
-  for (let i = 0; i < dirNumber-1; i++) {
+  for (let i = 0; i < dirNumber - 1; i++) {
     let direction = random(directions);
-    //randomList = createElement("p", direction);
-    //background(255);
 
-    if (number <= 25) {
+    if (number <= 24) {
       number = number + 1;
-      //randomList.position(100, textHeight);
-      text(direction, 100, textHeight);
+      text(direction, greeting.x, textHeight);
       textHeight = textHeight + 20;
-      state = 3;
-    } else if (number >= 26 && number <= 49) {
+    } else if (number >= 25 && number <= 49) {
       number = number + 1;
-      //randomList.position(300, textHeightc2);
-      text(direction, 300, textHeightc2);
+      text(direction, greeting.x+200, textHeightc2);
       textHeightc2 = textHeightc2 + 20;
-      state = 3;
-    } else if (number >= 50) {
+    } else if (number >= 50 && number<=74) {
       number = number + 1;
-      //randomList.position(500, textHeightc3);
-      text(direction, 500, textHeightc3);
+      text(direction, greeting.x+400, textHeightc3);
       textHeightc3 = textHeightc3 + 20;
-      state = 3;
+    }else if (number >= 75) {
+      number = number + 1;
+      text(direction, greeting.x+600, textHeightc4);
+      textHeightc4 = textHeightc4 + 20;
     }
   }
   buttonRestart = createButton("restart");
